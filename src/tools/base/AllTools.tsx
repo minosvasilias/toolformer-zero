8d02e21d470ed3bb1e40c6525b95f455590ca713
Markus <44006014+minosvasilias@users.noreply.github.com>
2023-03-01 01:07:26 +0100

    Add WolframAlpha Short Answers tool

--- src/tools/base/AllTools.tsx
+++ src/tools/base/AllTools.tsx
@@ -2,8 +2,9 @@ import { Google } from "../Google";
 import { MathJS } from "../MathJS";
 import { Now } from "../Now";
 import { SpotifyPlaylist } from "../SpotifyPlaylist";
+import { WolframAlpha } from "../WolframAlpha";
 import { Tool } from "./Tool";
 
 export const allTools: Array<
 	new (onResult?: Function, onError?: Function) => Tool
-> = [Google, MathJS, Now, SpotifyPlaylist];
+> = [Google, MathJS, Now, WolframAlpha, SpotifyPlaylist];
