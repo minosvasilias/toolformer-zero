236d2acd7458c101fd172667963e28ea25377d0c
Markus <44006014+minosvasilias@users.noreply.github.com>
2023-04-05 01:41:46 +0200

    Adapt prompt for chat completions

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -5,6 +5,7 @@ export const defaultTools: Array<string> = ["Google Search", "Math JS", "Now"];
 export const prompt_template = `You are an AI assistant with several tools available to you. The tools are the following:
 TOOL_DEFINITIONS
 DO NOT USE TOOLS WITHIN TOOLS! KEEP ALL TOOL CALLS SEPARATE FROM EACH OTHER!
+Make sure to continue the given prompt where it was left off. After a tool was used, make sure to continue the sentence as it was BEFORE the tool call.
 
 TOOL_EXAMPLES
 User: USER_INPUT
